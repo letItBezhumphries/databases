8bbbbddb9d1da4f0f23f9e47fc1654cf1c6a16ee
EricZH <letItBezhumphries>
2018-12-17 13:16:48 -0800

    Complete designing multi-table schema

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,6 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+mysql.createConnection({
 
+});
--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,38 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE `users` (
+`id` INTEGER,
+  `name` VARCHAR(25),
+  `messages` INTEGER(7),
+  PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `friends` (
+  `id` INTEGER,
+  `name` VARCHAR(25),
+  `user` INTEGER(10),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `messages` (
+  `id` INTEGER,
+  `text` VARCHAR(255),
+  `user` INTEGER(10),
+  `room` INTEGER,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `rooms` (
+  `id` INTEGER,
+  `roomName` VARCHAR(25),
+  `messages` INTEGER,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `friends` ADD FOREIGN KEY (user) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (user) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room) REFERENCES `rooms` (`id`);
 
 /* Create other tables and define schemas for them here! */
 
